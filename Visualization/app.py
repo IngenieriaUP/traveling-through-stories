19e27dabf3e64c4fbca133bc80dcfafd94073ce5
Ryu5e1 <54819428+Ryu5e1@users.noreply.github.com>
2019-11-15 08:59:53 -0500

    update app and added summary model

--- Visualization/app.py
+++ Visualization/app.py
@@ -2,11 +2,20 @@
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
-
+from sumy.parsers.html import HtmlParser
+from sumy.parsers.plaintext import PlaintextParser
+from sumy.nlp.tokenizers import Tokenizer
 import spacy
 from spacy import displacy
+from sumy.utils import get_stop_words
+from sumy.summarizers.text_rank import TextRankSummarizer
+from sumy.nlp.stemmers import Stemmer
+LANGUAGE = "spanish"
+SENTENCES_COUNT = 10
+import pandas as pd
+import re
 
-nlp = spacy.load("en_core_web_sm")
+nlp = spacy.load("../Research/models1")
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
@@ -35,13 +44,16 @@ app.layout = html.Div(children=[
         html.H2("Análisis de dependencias de sintaxis"),
         html.Iframe(id='viz_dep', srcDoc='', height="500p", width="100%"),
         html.H2("Análisis de entidades"),
-        html.Iframe(id='viz_ent', srcDoc='', height="500p", width="100%")
+        html.Iframe(id='viz_ent', srcDoc='', height="500p", width="100%"),
+        html.H2('Resumen de texto (Algoritmo Textrank)'),
+        html.Iframe(id='viz_sum', srcDoc='', height="500p", width="100%")
     ], style={'margin':'2%'})
 ])
 
 @app.callback(
     [dash.dependencies.Output('viz_dep', 'srcDoc'),
-     dash.dependencies.Output('viz_ent', 'srcDoc')],
+     dash.dependencies.Output('viz_ent', 'srcDoc'),
+     dash.dependencies.Output('viz_sum', 'srcDoc')],
     [dash.dependencies.Input('button', 'n_clicks')],
     [dash.dependencies.State('input-text', 'value')])
 def update_viz_dep(n_clicks, value):
@@ -51,8 +63,16 @@ def update_viz_dep(n_clicks, value):
     doc = nlp(value)
     dep_viz = displacy.render([doc], style="dep", page=False)
     ent_viz = displacy.render([doc], style="ent", page=False)
-
-    return dep_viz, ent_viz
+    parser = PlaintextParser.from_string(value,Tokenizer(LANGUAGE))
+    stemmer = Stemmer(LANGUAGE)
+    summarizer_text = TextRankSummarizer(stemmer)
+    resumen=''
+    summarizer_text.stop_words = get_stop_words(LANGUAGE)
+    summary_1 =summarizer_text(parser.document,SENTENCES_COUNT)
+    for sentence in summary_1:
+    	resumen=resumen+ str(sentence)+'\n'
+    	
+    return dep_viz, ent_viz, resumen
 
 if __name__ == '__main__':
     app.run_server(debug=True)
