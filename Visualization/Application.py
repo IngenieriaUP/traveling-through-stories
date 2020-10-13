64b3384c5c0849377b9e9a9bcbbbf5f1ba953b3f
Claudio Paul Ortega Ariza <claudio.rtega2701@gmail.com>
2020-10-13 12:35:56 -0500

    Update Application.py

--- Visualization/Application.py
+++ Visualization/Application.py
@@ -29,7 +29,7 @@ def darcolor(doc):
 def load_model(name):
     nlp=spacy.load(name)
     return nlp
-nlp = load_model('models1')
+nlp = load_model('Visualization/models1')
 HTML_WRAPPER = """<div style="overflow-x: auto; border: 1px solid #e6e9ef; border-radius: 0.25rem; padding: 1rem; margin-bottom: 2.5rem">{}</div>"""
 LANGUAGE = "spanish"
 #leer archivo
